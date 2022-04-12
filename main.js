1f7a1a9a0432285628fc9c242bbcd40f9f2c10a2
tas9n <nizi.minato@gmail.com>
2022-04-13 04:21:44 +0900

    参加時役職付与追加

--- main.js
+++ main.js
@@ -1,24 +1,57 @@
-// This is "[22/69]OECU-Bot".
+const DEBUG = false;//true;
 
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+// imports
+const { IntegrationExpireBehavior } = require('discord-api-types/v10');
+const { Client, Intents, Permissions, Message, Role } = require('discord.js');
+const { token } = require('./config.json');
 
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS
+    ]
 });
 
+// ピンをアクティビティに表示
+client.once('ready', () => {
+    console.log(`Ready on ${client.user.tag}`);
+
+    const setActivity = () => {
+        client.user.setActivity({
+            name: `${client.ws.ping}ms で稼働中... ${(DEBUG ? '(デバッグモード)' : '')}`
+        });
+    };
+
+    setActivity();
+    setInterval(() => {
+        setActivity();
+    }, 5000);
+});
+
+// 参加時役職付与
+client.on("guildMemberAdd", async member => {
+    member.roles.add('962372777447424070');
+});
+
+// コマンド関連
 client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
+	if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
+	const { commandName } = interaction;
+
+    if (commandName == 'exit') {
+        if (DEBUG) {
+            if (interaction.member.permissions.has('ADMINISTRATOR')) {
+                await interaction.reply('終了!');
+                process.exit();
+            } else {
+                await interaction.reply('このコマンドを実行する権限がありません。');
+            }
+        } else {
+            await interaction.reply('このコマンドはDEBUGモードのみ使用できます。');
+        }
+    }
 });
 
-
-(() => {
-    //token
-    require('dotenv').config();
-    client.login(process.env.DISCORD_BOT_TOKEN);
-})();
+// ログイン
+client.login(token);
